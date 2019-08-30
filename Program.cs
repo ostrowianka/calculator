36a35e93296b018c3b6a3d24bebb200b7339b6f0
Anna Wolska <anna.emilia.wolska@gmail.com>
2019-08-30 21:36:52 +0200

    Addition

--- Program.cs
+++ Program.cs
@@ -11,6 +11,31 @@ namespace calculator
             Console.WriteLine("2. Odejmowanie");
             Console.WriteLine("3. Mnożenie");
             Console.WriteLine("4. Dzielenie");
+
+            int choose = 0;
+            double a= 0, b=0;
+            choose=int.Parse(Console.ReadLine());
+            switch (choose)
+            {
+                case 1:
+                {
+                    Console.WriteLine("Podaj pierwszą liczbę");
+                    a = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Podaj drugą liczbę");
+                    b = double.Parse(Console.ReadLine());
+
+                    double result = Addition(a, b);
+                    Console.WriteLine($"Wynik dodawania: {result}");
+                    break;
+                }
+               
+            }
         }
+        public static double Addition (double a, double b)
+        {
+            double result = a+ b;
+            return result;
+        }
+       
     }
 }
